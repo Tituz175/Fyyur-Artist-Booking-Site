4bcc585e6db8901f871fefe1502292e833db3dba
Tituz175 <tobititus01@gmail.com>
2022-08-12 22:44:53 +0100

    db model completed

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+import sys
+from sqlalchemy import null
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -38,8 +40,20 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    genres = db.Column(db.PickleType)
     facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(500))
+    looking_for_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(), nullable=True)
+    upcoming_shows = db.Column(db.PickleType, nullable=True)
+    past_shows = db.Column(db.PickleType, nullable=True)
+    past_shows_count = db.Column(db.Integer, default=0, nullable=True)
+    upcoming_shows_count = db.Column(db.Integer,default=0, nullable=True)
+    
+    def __repr__(self) -> str:
+        return f'id:{self.id} name:{self.name} city: {self.city}'
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -54,10 +68,110 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(500))
+    looking_for_venues = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(), nullable=True)
+    upcoming_shows = db.Column(db.PickleType, nullable=True, default=[])
+    past_shows = db.Column(db.PickleType, nullable=True, default=[])
+    past_shows_count = db.Column(db.Integer, default=0, nullable=True)
+    upcoming_shows_count = db.Column(db.Integer,default=0, nullable=True)
+
+    def __repr__(self) -> str:
+        return f'id:{self.id} name:{self.name} city: {self.city}'
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.String())
+
+    def updateRecords(self,Artist,Venue):
+
+      artist = Artist.query.get(self.artist_id)
+      venue = Venue.query.get(self.venue_id)
+      if artist != null and venue != null:
+        venueRecordArtist = {
+        "artist_id": self.artist_id,
+        "artist_name": artist.name,
+        "artist_image_link": artist.image_link,
+        "start_time": self.start_time
+        }
+
+        artistRecordVenue = {
+        "venue_id": self.venue_id,
+        "venue_name": venue.name,
+        "venue_image_link": venue.image_link,
+        "start_time": self.start_time
+        }
+
+        date = format_datetime(self.start_time)
+        dateArray = date.split(',')
+        year = dateArray[-1][:5]
+    
+        if int(year) < int(datetime.today().year):
+          try:
+            artistData = list(artist.past_shows)
+            venueData = list(venue.past_shows)
+            artistData.append(artistRecordVenue)
+            venueData.append(venueRecordArtist)
+            currentCount = int(artist.past_shows_count) + 1
+            venueCurrentCount = int(venue.past_shows_count) + 1
+            artist.past_shows = artistData
+            artist.past_shows_count = currentCount
+            venue.past_shows = venueData
+            venue.past_shows_count = venueCurrentCount
+            db.session.commit()
+            print('committed')
+            # print(db.query.all())
+          except:
+            print(sys.exc_info)
+          finally:
+            db.session.close()
+            print('ended')
+
+        elif int(year) > int(datetime.today().year):
+          try:
+            artistData = list(artist.upcoming_shows)
+            artistData.append(artistRecordVenue)
+            currentCount = int(artist.upcoming_shows_count) + 1
+            artist.upcoming_shows = artistData
+            artist.upcoming_shows_count = currentCount
+            venueData = list(venue.upcoming_shows)
+            venueData.append(venueRecordArtist)
+            venueCurrentCount = int(venue.upcoming_shows_count) + 1
+            venue.upcoming_shows = venueData
+            venue.upcoming_shows_count = venueCurrentCount
+            db.session.commit()
+          except:
+            print('there was an error--> ')
+          finally:
+            db.session.close()
+            print('ended')
+        else:
+          try:
+            artistData = list(artist.upcoming_shows)
+            artistData.append(artistRecordVenue)
+            currentCount = int(artist.upcoming_shows_count) + 1
+            artist.upcoming_shows = artistData
+            artist.upcoming_shows_count = currentCount
+            venueData = list(venue.upcoming_shows)
+            venueData.append(venueRecordArtist)
+            venueCurrentCount = int(venue.upcoming_shows_count) + 1
+            venue.upcoming_shows = venueData
+            venue.upcoming_shows_count = venueCurrentCount
+            db.session.commit()
+          except:
+            print('there was an error--> ')
+          finally:
+            db.session.close()
+            print('ended')
+            
+      else:
+        return 'invalid ids'
 
 #----------------------------------------------------------------------------#
 # Filters.
